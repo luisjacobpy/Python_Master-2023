a95fcfd256896e34126e72ac0e26f3ab26a48cf0
luisjacobpy <16004230@es.uveg.com.mx>
2023-05-18 22:44:40 -0600

    Add Modulos and packages

---
+++ Lesson11_Metodos_de_las_colecciones/6_ejercicio.py
@@ -0,0 +1,11 @@
+"""
+Crea una función modificar() que a partir de una lista de números realice las siguientes tareas sin modificar la original
+
+* Borra los elemntos duplicados
+* Elimina todos los numeros impares
+* Realizar una suma de todos los numeros que quedan
+* Añadir como primer elemento de la lista la suma realizada
+* Devolver la lista modificada
+
+
+"""
